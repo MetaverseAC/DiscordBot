cd4644b4610ccf9fe758c6eda98dac0f89e35c56
deca <62450933+decaprime@users.noreply.github.com>
2020-05-19 22:25:17 -0700

    Change to async runmode on Jenkins command

--- DiscordBot/Modules/JenkinsModule.cs
+++ DiscordBot/Modules/JenkinsModule.cs
@@ -20,13 +20,13 @@ namespace DiscordBot.Modules
 			_jenkins = jenkins;
 		}
 
-		[Command("redeploy")]
+		[Command("redeploy", RunMode = RunMode.Async)]
 		[Alias("update")]
 		[RequireRole(OPERATOR_ROLE_ID)]
 		public async Task RedeployAsync()
 		{
-			await ReplyAsync("Redeploying PTR with latest version.");
 			await _jenkins.RunJobAsync("Deploy_ACE_PTR");
+			await ReplyAsync("Redeploying PTR with latest version.");
 		}
 	}
 }

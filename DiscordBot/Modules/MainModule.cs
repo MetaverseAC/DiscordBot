8bf707acd89e14bd96c5267fe1484f13f96b33f2
deca <62450933+decaprime@users.noreply.github.com>
2020-05-23 00:58:47 -0700

    add !uptime

--- DiscordBot/Modules/MainModule.cs
+++ DiscordBot/Modules/MainModule.cs
@@ -1,8 +1,10 @@
 ﻿using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
+using Humanizer;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,7 +19,51 @@ namespace DiscordBot.Modules
 		{
 			_client = client;
 		}
+
 		const ulong OPERATOR_ROLE_ID = 702744926466736179;
+
+		public static class ShellHelper
+		{
+			public static string Bash(string cmd)
+			{
+				var escapedArgs = cmd.Replace("\"", "\\\"");
+
+				var process = new Process()
+				{
+					StartInfo = new ProcessStartInfo
+					{
+						FileName = "/bin/bash",
+						Arguments = $"-c \"{escapedArgs}\"",
+						RedirectStandardOutput = true,
+						UseShellExecute = false,
+						CreateNoWindow = true,
+					}
+				};
+				process.Start();
+				string result = process.StandardOutput.ReadToEnd();
+				process.WaitForExit();
+				return result;
+			}
+		}
+
+		[Command("uptime")]
+		public async Task UptimeAsync()
+		{
+			var processStart = Process.GetCurrentProcess().StartTime;
+			var procUptime = DateTime.Now.Subtract(processStart);
+
+			var hostUptime = ShellHelper.Bash("cat /proc/uptime");
+			var hostTimeStr = hostUptime.Substring(0, hostUptime.IndexOf(" "));
+			var hostTimeSeconds = double.Parse(hostTimeStr);
+			var hostTime = TimeSpan.FromSeconds(hostTimeSeconds);
+
+			var embed = new EmbedBuilder();
+			embed.WithTitle("Uptime");
+			embed.AddField("BOT", procUptime.Humanize(2), true);
+			embed.AddField("HOST", hostTime.Humanize(2), true);
+			await ReplyAsync(embed: embed.Build());
+		}
+
 		[Command("ping")]
 		public Task PingAsync() => ReplyAsync("pong!");
 
